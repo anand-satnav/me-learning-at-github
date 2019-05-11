0e1836a47f27115bc9208420c6c13925bec34707
anand-satnav <anand.satnav@gmail.com>
2019-05-11 08:47:06 +0000

    Ensured it qualifies pylint code check

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+""" Learning at Github
+
+    A simple program to help me understand how to navigate about Github
+
+    Author : me :)
+
+"""
+
 import sys
 import numpy as np
 
 for a in np.arange(10):
-  print "Hello World";
-
+    print("Hello World")
 
 sys.exit(0)
